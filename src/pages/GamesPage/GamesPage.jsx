4a4e8be38927a3f1e19e2ab64406be427d1a711d
IvanAyuso <ivanao0812@gmail.com>
2024-02-27 10:38:44 +0100

    Game carousel styled

--- src/pages/GamesPage/GamesPage.jsx
+++ src/pages/GamesPage/GamesPage.jsx
@@ -2,6 +2,8 @@ import React, { Fragment, useEffect } from "react";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Pagination from "react-bootstrap/Pagination";
 import Button from "react-bootstrap/Button";
 
 import useGames from "../../hooks/useGames.js";
@@ -17,9 +19,7 @@ const GamesPage = () => {
   const { showGamesOffCanvas, creatingGame } = useGames();
   return (
     <Fragment>
-      <div>
-        <GameCarousel />
-      </div>
+      <GameCarousel />
       <div>
         <Button
           variant="primary"
@@ -31,6 +31,43 @@ const GamesPage = () => {
       <Container className="py-5 h-100">
         <Row className="g-5">
           <Games />
+        </Row>
+        <Row>
+          <Col>
+            <div>
+              <Pagination>
+                <Pagination.First />
+                <Pagination.Prev />
+                <Pagination.Item>{1}</Pagination.Item>
+                <Pagination.Ellipsis />
+
+                <Pagination.Item>{10}</Pagination.Item>
+                <Pagination.Item>{11}</Pagination.Item>
+                <Pagination.Item active>{12}</Pagination.Item>
+                <Pagination.Item>{13}</Pagination.Item>
+                <Pagination.Item disabled>{14}</Pagination.Item>
+
+                <Pagination.Ellipsis />
+                <Pagination.Item>{20}</Pagination.Item>
+                <Pagination.Next />
+                <Pagination.Last />
+              </Pagination>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div>
+              <h2>RECOMMENDED</h2>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div>
+              <h2>LAST REVIEWS</h2>
+            </div>
+          </Col>
         </Row>
       </Container>
       <OffcanvasComponent>
