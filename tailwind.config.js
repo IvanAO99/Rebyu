529d8124c644248768225af48737815325b2e33d
Iván <ivanao0812@gmail.com>
2024-05-10 17:19:17 +0200

    Added a light/dark theme button.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       /* Custom class for DecorativeCounter image component -> Change white for the bg color*/
-      backgroundImage: theme => ({
-        'fade-edge': 'linear-gradient(to right, white 1%, transparent, white 99%)',
-      })
+      backgroundImage: (theme) => ({
+        "fade-edge":
+          "linear-gradient(to right, white 1%, transparent, white 99%)",
+      }),
     },
   },
   plugins: [],
