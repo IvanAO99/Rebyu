557829eb67d1b55104b1934e191421c7ed5b7476
Iván <ivanao0812@gmail.com>
2024-06-03 15:50:16 +0200

    feat: improve responsiveness across the app
    - Enhanced responsive design for better compatibility with various screen sizes
    - Adjusted TailwindCSS breakpoints and utility classes
    - Updated component layouts to ensure consistent appearance on different devices

--- src/components/ReviewsFilter.jsx
+++ src/components/ReviewsFilter.jsx
@@ -14,47 +14,48 @@ const ReviewsFilter = () => {
 
   return (
     <>
-      <div
-        className="flex flex-row justify-center items-center gap-5 mt-2 border-b-2 border-gray-200 px-5 pb-5 text-lg"
-        onClick={(event) => {
-          setActiveOption(event.target.id);
-          handleClick(event.target.id);
-        }}
-      >
-        <button type="button"></button>
-        <button
-          type="button"
-          id="all"
-          className={`border-b-2 px-5 py-2 ${
-            activeOption === "all"
-              ? "border-purple-600 text-gray-900 dark:text-gray-50"
-              : "border-transparent hover:border-purple-600 text-gray-900/50 hover:text-gray-900 dark:text-gray-50/50 hover:dark:text-gray-50"
-          } transition-all`}
+      <div className="w-full flex flex-col justify-center items-center">
+        <div
+          className="w-60 lg:w-full flex flex-row justify-center items-center gap-5 mt-2 border-b-2 border-gray-200 px-5 pb-5 text-lg overflow-x-scroll lg:overflow-x-auto"
+          onClick={(event) => {
+            setActiveOption(event.target.id);
+            handleClick(event.target.id);
+          }}
         >
-          All Reviews
-        </button>
-        <button
-          type="button"
-          id="positive"
-          className={`border-b-2 px-5 py-2 ${
-            activeOption === "positive"
-              ? "border-purple-600 text-gray-900 dark:text-gray-50"
-              : "border-transparent hover:border-purple-600 text-gray-900/50 hover:text-gray-900 dark:text-gray-50/50 hover:dark:text-gray-50"
-          } transition-all`}
-        >
-          Positive Reviews
-        </button>
-        <button
-          type="button"
-          id="negative"
-          className={`border-b-2 px-5 py-2 ${
-            activeOption === "negative"
-              ? "border-purple-600 text-gray-900 dark:text-gray-50"
-              : "border-transparent hover:border-purple-600 text-gray-900/50 hover:text-gray-900 dark:text-gray-50/50 hover:dark:text-gray-50"
-          } transition-all`}
-        >
-          Negative Reviews
-        </button>
+          <button
+            type="button"
+            id="all"
+            className={`border-b-2 px-5 py-2 ${
+              activeOption === "all"
+                ? "border-purple-600 text-gray-900 dark:text-gray-50"
+                : "border-transparent hover:border-purple-600 text-gray-900/50 hover:text-gray-900 dark:text-gray-50/50 hover:dark:text-gray-50"
+            } transition-all`}
+          >
+            All Reviews
+          </button>
+          <button
+            type="button"
+            id="positive"
+            className={`border-b-2 px-5 py-2 ${
+              activeOption === "positive"
+                ? "border-purple-600 text-gray-900 dark:text-gray-50"
+                : "border-transparent hover:border-purple-600 text-gray-900/50 hover:text-gray-900 dark:text-gray-50/50 hover:dark:text-gray-50"
+            } transition-all`}
+          >
+            Positive Reviews
+          </button>
+          <button
+            type="button"
+            id="negative"
+            className={`border-b-2 px-5 py-2 ${
+              activeOption === "negative"
+                ? "border-purple-600 text-gray-900 dark:text-gray-50"
+                : "border-transparent hover:border-purple-600 text-gray-900/50 hover:text-gray-900 dark:text-gray-50/50 hover:dark:text-gray-50"
+            } transition-all`}
+          >
+            Negative Reviews
+          </button>
+        </div>
       </div>
     </>
   );
